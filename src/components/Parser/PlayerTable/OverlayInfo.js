019bec51178146f049c76fa2e969462b0dcfa038
Innpaul Zhang <zyp108421@gmail.com>
2024-04-05 00:12:42 +0800

    chore: remove funding

--- src/components/Parser/PlayerTable/OverlayInfo.js
+++ src/components/Parser/PlayerTable/OverlayInfo.js
@@ -33,10 +33,6 @@ class OverlayInfo extends React.Component {
 			<div id='overlay-info' ref={el => (this.instance = el)}>
 				<h3>Ember Overlay</h3>
 				{this.getInfoText()}
-
-				<div id='funding'>
-					{this.getFundingText()}
-				</div>
 			</div>
 		);
 	}
@@ -63,77 +59,6 @@ class OverlayInfo extends React.Component {
 					</React.Fragment>
 				);
 		}
-	}
-
-	getFundingText() {
-		const type = (["hibiya", "actws"].indexOf(this.state.chrome) === -1 || window.obsstudio) ? "new" : "old";
-		const torn = Math.floor((Math.random() * 2) + 1);
-
-		switch (type) {
-			case "old":
-				return (
-					<React.Fragment>
-						<span onClick={this.openFundingLink.bind(this, "cash")} ref='cash'><img src='img/buttons/funding/cash.svg' alt='Donate on Cash App' height='20'/></span>
-						<span onClick={this.openFundingLink.bind(this, "paypal")} ref='paypal'><img src='img/buttons/funding/paypal-resized.png' alt='Donate at PayPal' height='20'/></span>
-						<span onClick={this.openFundingLink.bind(this, "paypay")} ref='paypay'><img src='img/buttons/funding/paypay.svg' alt='ペイペイで施してください' height='20'/></span>
-						<span onClick={this.openFundingLink.bind(this, "kofi")} ref='kofi'><img src='img/buttons/funding/kofi.svg' alt='Donate at Ko-fi' height='20'/></span>
-						<span onClick={this.openFundingLink.bind(this, "patreon")} ref='patreon'><img src='img/buttons/funding/patreon.png' alt='Donate at Patreon' height='20'/></span>
-						<span onClick={this.openFundingLink.bind(this, "streamlabs")} ref='streamlabs'><img src='img/buttons/funding/streamlabs.svg' alt='Donate at Streamlabs' height='20'/></span>
-					</React.Fragment>
-				);
-
-			case "new":
-			default:
-				return (
-					<React.Fragment>
-						<span onClick={this.openAdLink.bind(this)} ref='ad'><img className='ad-cta' src={`img/buttons/funding/torn${torn}.png`} alt='Support Ember by clicking here'/></span>
-						<p>Try out the text-based RPG Torn City.</p>
-					</React.Fragment>
-				);
-		}
-	}
-
-	openFundingLink(rel) {
-		let url = "";
-
-		// Min for PayPal: Hibiya
-		// Min for Streamlabs: ngld
-		// Min for Ko-fi: Hibiya
-		// Min for Patreon: ngld
-		switch (this.state.chrome) {
-			case "hibiya":
-			case "actws":
-				switch (rel) {
-					case "paypal":
-					case "kofi":
-					case "cash":
-					case "paypay":
-						url = DonationService.getRealDonationLink(rel);
-
-						break;
-
-					default:
-						url = DonationService.buildLocalDonationLink(rel);
-
-						break;
-				}
-
-				break;
-
-			case "ngld":
-			default:
-				url = DonationService.getRealDonationLink(rel);
-
-				break;
-		}
-
-		window.open(url, "", "width=600,height=530,location=yes,menubar=yes");
-	}
-
-	openAdLink() {
-		const url = "https://www.torn.com/1962321";
-
-		window.open(url, "", "width=1200,height=830,location=no,menubar=no");
 	}
 }
 
