69802b64080b4e80b335f66282e815ad602868a4
Innpaul Zhang <zyp108421@gmail.com>
2024-04-05 00:16:57 +0800

    chore: remove streamer

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -5,39 +5,19 @@ import { Grid, Menu } from "semantic-ui-react";
 
 import LocalizationService from "../services/LocalizationService";
 import SettingsSchema from "../constants/SettingsSchema";
-import TwitchAPIService from "../services/TwitchAPIService";
 
 import Screen from "./Settings/Screen";
 import Export from "./Settings/Export";
 import About from "./Settings/About";
-import Streamers from "./Settings/Streamers";
 import Donate from "./Settings/Donate";
 
 import "./../styles/components/settings/settings-theme.less";
 
 class Settings extends React.Component {
-	UNSAFE_componentWillMount() {
-		TwitchAPIService.getLiveStreamers()
-			.then(data => {
-				this.setState({
-					stream_type : data.type,
-					streamers   : data.streamers,
-				});
-			})
-			.catch(() => {});
-	}
-
 	render() {
-		const base_url           = this.props.match.url;
-		const nav_links          = [];
-		const routes             = [];
-		const streamers          = (this.state && this.state.streamers) ? this.state.streamers : {};
-		const stream_type        = (this.state && this.state.stream_type) ? this.state.stream_type : "offline";
-		const streamer_count     = (streamers) ? Object.keys(streamers).length : 0;
-		const streamer_base_text = (streamer_count && stream_type === "live")
-			? LocalizationService.getMisc((streamer_count > 1) ? "streamers_live" : "streamer_live")
-			: LocalizationService.getMisc("streamers");
-		const streamer_text      = streamer_base_text.replace("{{number}}", streamer_count);
+		const base_url  = this.props.match.url;
+		const nav_links = [];
+		const routes    = [];
 
 		for (
 			const section of
@@ -69,7 +49,6 @@ class Settings extends React.Component {
 									{nav_links}
 									<NavLink to={base_url + "/export"} className='item'>{LocalizationService.getSettingsSectionText("export")}</NavLink>
 									<NavLink to={base_url + "/about"} className='item'>{LocalizationService.getSettingsSectionText("about")}</NavLink>
-									<NavLink to={base_url + "/streamers"} className='streamers item'>{streamer_text}</NavLink>
 									<NavLink to={base_url + "/donate"} className='item'>Donate</NavLink>
 								</Menu>
 							</Grid.Column>
@@ -80,7 +59,6 @@ class Settings extends React.Component {
 								{routes}
 								<Route path={base_url + "/export"} component={Export}/>
 								<Route path={base_url + "/about"} component={About}/>
-								<Route path={base_url + "/streamers"} render={() => <Streamers streamers={streamers} type={stream_type}/>}/>
 								<Route path={base_url + "/donate"} component={Donate}/>
 							</Grid.Column>
 						</Grid.Row>
